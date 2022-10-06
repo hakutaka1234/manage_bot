6ae82432219158446a90752879958ed68dce2ca0
hakutaka1234 <rachmad.alfio@gmail.com>
2022-10-06 20:15:34 +0700

    Manage-Bot

---
+++ HakutakaRobot/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from HakutakaRobot.events import register
+from HakutakaRobot import telethn as tbot
+from HakutakaRobot import BOT_USERNAME as bu
+
+PHOTO = None
+
+@register(pattern=("/alive"))
+async def awake(event):
+  Haku = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ʜᴀᴋᴜ** \n\n"
+  Haku += "✪ **I'm Working Properly** \n\n"
+  Haku += f"✪ **My Master : [〲༆ʜᴀᴋᴜ ᴀʟɪᴠᴇ ༇〺](https://t.me/HakutakaRobot)** \n\n"
+  Haku += f"✪ **Library Version :** `{telever}` \n\n"
+  Haku += f"✪ **Telethon Version :** `{tlhver}` \n\n"
+  Haku += f"✪ **Pyrogram Version :** `{pyrover}` \n\n"
+  Haku += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", f"https://t.me/{bu}?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/alivenotalliance")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=Haku,  buttons=BUTTON)
---
+++ README.md
@@ -0,0 +1,21 @@
+<h4><p align="center"> 〲༆ʜᴀᴋᴜ ᴀʟɪᴠᴇ ༇〺 </p></h4>
+
+<p align="center">
+
+  <img src="https://graph.org/file/a4d8ec008094e0e71cbda.jpg">
+
+</p>
+
+<p align="center">
+
+<a href="https://t.me/gemini_hakutakaa"> <img src="https://img.shields.io/badge/Haku-Robot-blue?&logo=telegram" alt="HakuRobot on Telegram" /> </a><br>
+
+<a href="https://python-telegram-bot.org"> <img src="https://img.shields.io/badge/PTB-13.9.11-white?&style=flat-round&logo=github" alt="Python Telegram Bot" /> </a>
+
+<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a><br>
+
+<a href="https://docs.telethon.dev"> <img src="https://img.shields.io/badge/Telethon-1.24.0-red?&style=flat-round&logo=github" alt="Telethon" /> </a>
+
+<a href="https://docs.python.org"> <img src="https://img.shields.io/badge/Python-3.10.4-purple?&style=flat-round&logo=python" alt="Python" /> </a><br>
+
+</p>
