b71409cff5ff3dc570ebc33ab77bd30612b57a77
HakutakaRobot <63560321+hakutaka1234@users.noreply.github.com>
2022-10-07 07:15:34 +0700

    Update alive.py

--- HakutakaRobot/modules/alive.py
+++ HakutakaRobot/modules/alive.py
@@ -8,7 +8,7 @@ from pyrogram import __version__ as pyrover
 from HakutakaRobot.events import register
 from HakutakaRobot import telethn as tbot
 from HakutakaRobot import BOT_USERNAME as bu
-
+PHOTO = "https://graph.org/file/a4d8ec008094e0e71cbda.jpg"
 @register(pattern="^/alive ?(.*)")
 async def awake(event):
   Haku = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ʜᴀᴋᴜ** \n\n"
@@ -19,4 +19,4 @@ async def awake(event):
   Haku += f"㊝ **Pyrogram Version :** `{pyrover}` \n\n"
   Haku += "**Thanks For Adding Me Here**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", f"https://t.me/{bu}?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/alivenotalliance")]]
-  await tbot.client.get_messages(event.chat_id, caption=Haku,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, PHOTO=None, caption=Haku,  buttons=BUTTON)
